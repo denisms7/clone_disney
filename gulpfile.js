0229f324b9cec2c6d86dfe5d69b359393b876f0a
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-01-14 19:36:56 -0300

    added img

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -9,7 +9,17 @@ function styles() {
 }
 
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'))
+}
+
+
+
+exports.default = gulp.parallel(styles, images)
+
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
